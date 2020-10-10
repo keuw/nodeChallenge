583d42e19fd8f63209f98dfe866c9e41cd4fccab
Tony Kung <tonykung1997@hotmail.com>
2020-10-10 11:36:38 -0400

    Updates

---
+++ precache-manifest.6b13670b3583d2da67e4364bd97dcf87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5790258066281f720ab5ea1348768d",
+    "url": "/nodeChallenge/index.html"
+  },
+  {
+    "revision": "0fe0196361cb29b1a986",
+    "url": "/nodeChallenge/static/css/main.8c80a406.chunk.css"
+  },
+  {
+    "revision": "1c8cc52e49a623f1c54a",
+    "url": "/nodeChallenge/static/js/2.28f7f4c3.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/nodeChallenge/static/js/2.28f7f4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe0196361cb29b1a986",
+    "url": "/nodeChallenge/static/js/main.1caadff7.chunk.js"
+  },
+  {
+    "revision": "7c10fea0ff0136f9360e",
+    "url": "/nodeChallenge/static/js/runtime-main.d3c7c2b4.js"
+  },
+  {
+    "revision": "44e8b105565f2e704bfd3dc977c42a40",
+    "url": "/nodeChallenge/static/media/profile.44e8b105.png"
+  }
+]);
